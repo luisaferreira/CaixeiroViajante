4f4e7706c56153a252bb4c0ba14c42bec9cf6e88
Marina Flores <mlbtf@discente.ifpe.edu.br>
2024-08-25 17:34:00 -0300

    Implementando sele ção de individuos

--- Program.cs
+++ Program.cs
@@ -7,10 +7,25 @@
             try
             {
                 int qtdCidades = XMLHelper.ObterNumeroDeCidades();
-                int qtdIndividuos = 6;
+                int qtdIndividuos = 10;
+                // TODO: Definir número máximo de gerações
+                // TODO: Definir fitness de parada
 
                 var algoritmo = new Algoritmo(qtdIndividuos, qtdCidades);
                 algoritmo.Populacao.ImprimirPopulacao();
+
+                // TODO: Validar se melhor indivíduo se encaixa no fitness de parada
+                var melhorIndividuo = algoritmo.ObterMelhorIndividuo();
+
+                // Seleção 
+                var selecionados = algoritmo.SelecionarIndividuos();
+
+                // TODO: Cruzamento
+
+                // TODO: Mutação
+
+                // TODO: Atualizar população
+
             }
             catch (Exception ex)
             {
